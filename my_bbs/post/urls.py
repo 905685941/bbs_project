1036ce336443080801271ce4df8f7553396d95cf
JianQiang Guan <root@192.168.198.128>
2022-04-07 15:44:11 +0800

    feat: 第六章功能实现，views使用

---
+++ my_bbs/post/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from post import views
+
+urlpatterns = [
+    path("topic_list/", views.topic_list_view),
+    path("topic_detail/<int:topic_id>/", views.topic_detail_view),
+    path("topic_comment/", views.add_comment_to_topic_view)
+]
--- my_bbs/post/views.py
+++ my_bbs/post/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from post.models import Topic, Comment
+from post.post_service import build_topic_base_info, build_topic_detail_info, add_comment_to_topic
 
 # Create your views here.
+def topic_list_view(request):
+    "话题列表"
+    topic_qs = Topic.objects.all()
+    result = {
+        "count": topic_qs.count(),
+        "info": [build_topic_base_info(topic) for topic in topic_qs]
+    }
+    return JsonResponse(result)
+
+
+def topic_detail_view(request, topic_id):
+    """话题详情"""
+    result = []
+    try:
+        result = build_topic_detail_info(Topic.objects.get(pk=topic_id))
+    except Topic.DoesNotExist:
+        pass
+    print(result)
+    return JsonResponse(result)
+
+
+@csrf_exempt
+def add_comment_to_topic_view(request):
+    """
+    给话题添加评论
+    """
+    topic_id = int(request.POST.get("id",0))
+    content = request.POST.get("content", "")
+    topic = None
+    try:
+        topic = Topic.objects.get(pk=topic_id)
+    except Topic.DoesNotExist:
+        pass
+
+    if topic and content:
+        return JsonResponse({"id": add_comment_to_topic(topic, content).id})
+    return JsonResponse({})
